59cd0b1dffc60ad04677e52fc4df68a58ab1f21a
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2020-10-12 23:14:08 +0200

    2.0

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int n;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+    for (int j = 1; j < n; j++) {
+        int i = j - 1;
+        int elem = array[j];
+        while (i >= 0 && array[i] > elem) {
+            array[i + 1] = array[i];
+            i--;
+        }
+        array[i + 1] = elem;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
 }
