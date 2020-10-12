6564f3ecda07dfe90420617e3e5a0e6cde6a07c2
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2020-10-13 00:30:01 +0200

    3.1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
+
 class INSERTION_SORT_CLASS;
+
 class TEST_CLASS;
 
 class INSERTION_SORT_CLASS {
@@ -11,6 +13,7 @@ class INSERTION_SORT_CLASS {
     int *arrayMOD;
 
     void copyArray() { for (int i = 0; i < elem; i++) { arrayMOD[i] = array[i]; }}
+
 public:
     INSERTION_SORT_CLASS(int *array, int elem) {
         this->array = array;
@@ -19,6 +22,10 @@ public:
         copyArray();
     }
 
+    ~INSERTION_SORT_CLASS() {
+        delete[] array;
+        delete[] arrayMOD;
+    }
 
     void displayArray() {
         for (int i = 0; i < elem; i++) {
@@ -38,41 +45,90 @@ public:
             arrayMOD[i + 1] = elem;
         }
     }
+
+    int *ResortArray() {
+        return arrayMOD;
+    }
+
     friend TEST_CLASS;
 };
-class TEST_CLASS{
-    INSERTION_SORT_CLASS* testObj;
-    TEST_CLASS(INSERTION_SORT_CLASS* testObj){
-        this->testObj=testObj;
+
+class TEST_CLASS {
+
+    int testNumber;
+public:
+    TEST_CLASS(int testNumber) {
+
+        this->testNumber = testNumber;
+        TesterMaster(testNumber);
     }
-    ~TEST_CLASS(){
+
+private:
+    void DisplayMessage(char *message) {
+        printf("%s\n", message);
+    }
+
+    void DisplayArray(int *array, int size) {
+        for (int i = 0; i < size; i++) { printf("%d " "", array[i]); }
+        printf("\n");
+    }
+
+    bool compare(int *array1, int *array2, int size) {
+        for (int i = 0; i < size; i++) {
+            if (array1[i] != array2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void extraBubbleSort(int *array, int n) {
+        int i, j;
+        for (i = 0; i < n - 1; i++)
+            for (j = 0; j < n - i - 1; j++)
+                if (array[j] > array[j + 1])
+                    std::swap(array[j], array[j + 1]);
+    }
+
+    void Test() {
+        srand(time(NULL));
+        int size = rand() % 10 + 200;
+        int *array = new int[size];
+        int *arrayrecived;
+        for (int i = 0; i < size; i++) { array[i] = rand() % 1000; }
+        INSERTION_SORT_CLASS *testObj = new INSERTION_SORT_CLASS(array, size);
+        testObj->sort();
+        arrayrecived = testObj->ResortArray();
+        extraBubbleSort(array, size);
+        if (compare(arrayrecived, array, size)) {
+            printf("%s", "Array derived:\n");
+            DisplayArray(arrayrecived, size);
+            printf("%s", "Array poprerty sorted:\n");
+            DisplayArray(array, size);
+            DisplayMessage("PASS");
+
+        } else {
+            printf("%s", "Array derived:\n");
+            DisplayArray(arrayrecived, size);
+            printf("%s", "Array poprerty sorted:\n");
+            DisplayArray(array, size);
+            DisplayMessage("FAIL");
+        }
+        delete[] array;
+        delete[] arrayrecived;
         delete testObj;
-        delete [] array;
+
     }
-    int *array=new int [testObj->elem];
-    void copy(){
-        for(int i = 0; i < testObj->elem; i++){
-            array[i]=testObj->array[i];
+
+    void TesterMaster(int testNumbers) {
+        for (int i = 0; i < testNumbers; i++) {
+            printf("\n%s%d%s\n\n", "--------------------------------TEST ID: ", i,"--------------------------------");
+            Test();
         }
     }
-    void extraBubbleSort(){
-        int i, j;
-        int n = testObj->elem;
-        for (i = 0; i < n-1; i++)
-            for (j = 0; j < n-i-1; j++)
-                if (array[j] > array[j+1])
-                    std::swap(array[j], array[j+1]);
-    }
-
-
-
-
 
 };
+
 int main() {
-    int array[50];
-    for(int i = 0; i<50; i++){ array[i]=rand() %1000;}
-  INSERTION_SORT_CLASS* obj1 = new INSERTION_SORT_CLASS (array,50);
-    obj1->displayArray();
-    delete obj1;
+    TEST_CLASS s(4);
 }
