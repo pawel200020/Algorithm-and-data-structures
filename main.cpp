b846a6b41da580497bd5bc8272082b8bec9d6c8d
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2020-11-03 18:59:28 +0100

    3.3 Ready

--- main.cpp
+++ main.cpp
@@ -5,130 +5,188 @@
 
 class INSERTION_SORT_CLASS;
 
+class HEAP_SORT_CLASS;
+
 class TEST_CLASS;
 
-class INSERTION_SORT_CLASS {
-    const int *array;
-    int elem;
-    int *arrayMOD;
+class HEAP_SORT_CLASS {
 
-    void copyArray() { for (int i = 0; i < elem; i++) { arrayMOD[i] = array[i]; }}
+    void heapify(int *array, int size, int i) {
+        int largest = i;
+        int left_i = 2 * i + 1;
+        int right_i = 2 * i + 2;
+
+        if (left_i < size && array[left_i] > array[largest]) {
+            largest = left_i;
+        }
+        if (right_i < size && array[right_i] > array[largest]) {
+            largest = right_i;
+        }
+        if (largest != i) {
+            std::swap(array[i], array[largest]);
+            heapify(array, size, largest);
+        }
+    }
+
+    void bulid_heap(int size, int *array) {
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(array, size, i);
+        }
+    }
+public:
+    void heapSort(int *array, int size) {
+        bulid_heap(size, array);
+        for (int i = size - 1; i > 0; i--) {
+            std::swap(array[0], array[i]);
+            heapify(array, i, 0);
+        }
+    }
+};
+
+class MERGE_SORT_CLASS {
+
+
+    void merge(int arr[], int left, int divide, int right) {
+        int array1size = divide - left + 1;
+        int array2size = right - divide;
+        int *Left = new int[array1size];
+        int *Right = new int[array2size];
+
+        for (int i = 0; i < array1size; i++)
+            Left[i] = arr[left + i];
+        for (int i = 0; i < array2size; i++)
+            Right[i] = arr[divide + 1 + i];
+
+        int firstbegin = 0;
+
+        int secbegin = 0;
+
+        int k = left;
+
+        while (firstbegin < array1size && secbegin < array2size) {
+            if (Left[firstbegin] <= Right[secbegin]) {
+                arr[k] = Left[firstbegin];
+                firstbegin++;
+            } else {
+                arr[k] = Right[secbegin];
+                secbegin++;
+            }
+            k++;
+        }
+
+        while (firstbegin < array1size) {
+            arr[k] = Left[firstbegin];
+            firstbegin++;
+            k++;
+        }
+        while (secbegin < array2size) {
+            arr[k] = Right[secbegin];
+            secbegin++;
+            k++;
+        }
+        delete[] Left;
+        delete[] Right;
+    }
+public:
+    void mergeSort(int *array, int left, int right) {
+        if (left < right) {
+            int divide = left + (right - left) / 2;
+            mergeSort(array, left, divide);
+            mergeSort(array, divide + 1, right);
+            merge(array, left, divide, right);
+        }
+    }
+};
+
+class INSERTION_SORT_CLASS {
 
 public:
-    INSERTION_SORT_CLASS(int *array, int elem) {
-        this->array = array;
-        this->elem = elem;
-        this->arrayMOD = new int[elem];
-        copyArray();
-    }
 
-    ~INSERTION_SORT_CLASS() {
-        delete[] array;
-        delete[] arrayMOD;
-    }
-
-    void displayArray() {
-        for (int i = 0; i < elem; i++) {
-            printf("%d"" ", arrayMOD[i]);
-        }
-        printf("\n");
-    }
-
-    void sort() {
-        for (int j = 1; j < elem; j++) {
+    void sort(int *array, int size) {
+        for (int j = 1; j < size; j++) {
             int i = j - 1;
-            int elem = arrayMOD[j];
-            while (i >= 0 && arrayMOD[i] > elem) {
-                arrayMOD[i + 1] = arrayMOD[i];
+            int elem = array[j];
+            while (i >= 0 && array[i] > elem) {
+                array[i + 1] = array[i];
                 i--;
             }
-            arrayMOD[i + 1] = elem;
+            array[i + 1] = elem;
         }
     }
-
-    int *ResortArray() {
-        return arrayMOD;
-    }
-
-    friend TEST_CLASS;
 };
 
 class TEST_CLASS {
-
-    int testNumber;
 public:
-    TEST_CLASS(int testNumber) {
+    int numberOfTests;
+    int sizeOfArray;
 
-        this->testNumber = testNumber;
-        TesterMaster(testNumber);
+    TEST_CLASS(int numberOfTests, int sizeOfArray) {
+        this->numberOfTests = numberOfTests;
+        this->sizeOfArray = sizeOfArray;
     }
 
 private:
-    void DisplayMessage(char *message) {
-        printf("%s\n", message);
-    }
-
-    void DisplayArray(int *array, int size) {
-        for (int i = 0; i < size; i++) { printf("%d " "", array[i]); }
-        printf("\n");
-    }
-
-    bool compare(int *array1, int *array2, int size) {
+    void displayArray(int *array, int size) {
         for (int i = 0; i < size; i++) {
-            if (array1[i] != array2[i]) {
-                return false;
+            printf("%d ", array[i]);
+        }
+        printf("\n");
+
+    }
+
+public:
+    void test(char algorithm) {
+        srand(time(nullptr));
+        int **HugeTestArray = new int *[numberOfTests];
+        for (int i = 0; i < numberOfTests; i++) {
+            HugeTestArray[i] = new int[sizeOfArray];
+            for (int j = 0; j < sizeOfArray; j++) {
+                HugeTestArray[i][j] = rand() % 100;
             }
+
         }
-        return true;
-    }
+        switch (algorithm) {
+            case 'h': {
+                HEAP_SORT_CLASS h1;
+                for (int i = 0; i < numberOfTests; i++) {
+                    printf("Test id: %d \n", i);
+                    displayArray(HugeTestArray[i], sizeOfArray);
+                    h1.heapSort(HugeTestArray[i], sizeOfArray);
+                    displayArray(HugeTestArray[i], sizeOfArray);
+                    printf("\n");
+                }
+                case 'i': {
+                    INSERTION_SORT_CLASS i1;
+                    for (int i = 0; i < numberOfTests; i++) {
+                        printf("Test id: %d \n", i);
+                        displayArray(HugeTestArray[i], sizeOfArray);
+                        i1.sort(HugeTestArray[i], sizeOfArray);
+                        displayArray(HugeTestArray[i], sizeOfArray);
+                        printf("\n");
+                    }
+                }
+                case 'm': {
+                    MERGE_SORT_CLASS m1;
+                    for (int i = 0; i < numberOfTests; i++) {
+                        printf("Test id: %d \n", i);
+                        displayArray(HugeTestArray[i], sizeOfArray);
+                        m1.mergeSort(HugeTestArray[i], 0, sizeOfArray);
+                        displayArray(HugeTestArray[i], sizeOfArray);
+                        printf("\n");
+                    }
+                }
+            }
 
-    void extraBubbleSort(int *array, int n) {
-        int i, j;
-        for (i = 0; i < n - 1; i++)
-            for (j = 0; j < n - i - 1; j++)
-                if (array[j] > array[j + 1])
-                    std::swap(array[j], array[j + 1]);
-    }
 
-    void Test() {
-        int size = rand() % 10 + 200;
-        int *array = new int[size];
-        int *arrayrecived;
-        for (int i = 0; i < size; i++) { array[i] = rand() % 1000; }
-        INSERTION_SORT_CLASS *testObj = new INSERTION_SORT_CLASS(array, size);
-        testObj->sort();
-        arrayrecived = testObj->ResortArray();
-        extraBubbleSort(array, size);
-        if (compare(arrayrecived, array, size)) {
-            printf("%s", "Array derived:\n");
-            DisplayArray(arrayrecived, size);
-            printf("%s", "Array poprerty sorted:\n");
-            DisplayArray(array, size);
-            DisplayMessage("PASS");
-
-        } else {
-            printf("%s", "Array derived:\n");
-            DisplayArray(arrayrecived, size);
-            printf("%s", "Array poprerty sorted:\n");
-            DisplayArray(array, size);
-            DisplayMessage("FAIL");
         }
-        delete[] array;
-        delete[] arrayrecived;
-        delete testObj;
 
-    }
-
-    void TesterMaster(int testNumbers) {
-        srand(time(NULL));
-        for (int i = 0; i < testNumbers; i++) {
-            printf("\n%s%d%s\n\n", "--------------------------------TEST ID: ", i,"--------------------------------");
-            Test();
-        }
     }
 
 };
 
 int main() {
-    TEST_CLASS s(4);
+    TEST_CLASS t1(5, 50);
+    t1.test('h');
+
+
 }
